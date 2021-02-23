c20a6897c3fb41a0c353c99d35ad03d9d6a538fc
Spencer Christy <spenrad@gmail.com>
2021-02-22 17:31:03 -0800

    monkies reapeating on page as background image in divs

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React from "react";
 import Footer from "./components/footer";
 import Header from "./components/header";
-import Main from "./components/main";
 import Navbar from "./components/navbar";
 import Wrapper from "./components/wrapper";
 import monkey from "./monkey.json";
 import './App.css';
 
-function App() {
-  return (
+class App extends React.Component {
 
+  state = {
+    monkey: monkey
+  };
+
+render() {
+  return (
+    <Wrapper>
+      <Navbar></Navbar>
+      <Header></Header>
+      {this.state.monkey.map(function (monkey){
+      return (<div 
+        id={monkey.id}
+        name={monkey.name}
+        class="monkey"
+        style={{backgroundImage: `url(${monkey.image})`}} 
+        ></div>)})}
+      <Footer></Footer> 
+    </Wrapper>
   );
 }
-
+}
 export default App;
